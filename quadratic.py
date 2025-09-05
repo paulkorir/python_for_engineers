5dbb990f05c5bddf5fe4860eb0501d7c519dac49
paulkorir <paul@sci2pro.com>
2025-09-05 12:40:52 +0300

    defined function for computing solutions

--- quadratic.py
+++ quadratic.py
@@ -1,11 +1,8 @@
-import sys
 import cmath
+import sys
 
 
-def main():
-    a = complex(input("a: "))
-    b = complex(input("b: "))
-    c = complex(input("c: "))
+def find_solutions(a, b, c):
     # discriminant
     d = b ** 2 - 4 * a * c
     print(f"discriminant = {d}")
@@ -14,6 +11,14 @@ def main():
     # x2 = (-b - math.sqrt(d)) / (2 * a)
     x1 = (-b + cmath.sqrt(d)) / (2 * a)
     x2 = (-b - cmath.sqrt(d)) / (2 * a)
+    return x1, x2
+
+
+def main():
+    a = complex(input("a: "))
+    b = complex(input("b: "))
+    c = complex(input("c: "))
+    x1, x2 = find_solutions(a, b, c)
     print("The roots are:")
     print(f"x1 = {x1}")
     print(f"x2 = {x2}")
