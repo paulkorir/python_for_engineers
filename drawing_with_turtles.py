a7fc22dcfc5be01066c0d8811077421dff8812a4
paulkorir <paul@sci2pro.com>
2025-09-09 15:50:42 +0300

    draw a circle with fill and stroke

--- drawing_with_turtles.py
+++ drawing_with_turtles.py
@@ -16,8 +16,23 @@ def draw_rectangle():
     turtle.forward(110)
     turtle.done()
 
+def draw_circle():
+    pen = turtle.Turtle()
+    if pen.isdown():
+        pen.up()
+    pen.goto(15, 28)
+    pen.down()
+    pen.begin_fill()
+    pen.pencolor("red")
+    pen.fillcolor("purple")
+    pen.circle(60)
+    pen.end_fill()
+    pen.up()
+    turtle.done()
+
 def main():
-    draw_rectangle()
+    # draw_rectangle()
+    draw_circle()
     return 0
 
 
