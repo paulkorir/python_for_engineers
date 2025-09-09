7d49958e42b024cd755e26fe004e62b95ecc62b4
paulkorir <paul@sci2pro.com>
2025-09-09 15:21:43 +0300

    now circle has methods

--- oop_circle.py
+++ oop_circle.py
@@ -10,40 +10,36 @@ class Circle:
         self.position = position
         # create two attributes 'diameter' and 'area'
         self.diameter = 2 * radius
-        self.area = 3.142 * self.radius * self.radius
+        # self.area = 3.142 * self.radius * self.radius
 
     def __str__(self):
         return f"I am a circle of size {self.radius}{self.units} located at {self.position}."
 
+    def area(self):
+        return math.pi * self.radius ** 2
 
-def area(circle):
-    return math.pi * circle.radius ** 2
+    def perimeter(self):
+        return 2 * math.pi * self.radius
 
+    def arc_length(self, theta, radians=True):
+        if radians:
+            return theta * self.radius
+        else:
+            theta_radians = math.radians(theta)
+            return theta_radians * self.radius
 
-def perimeter(circle):
-    return 2 * math.pi * circle.radius
-
-
-def arc_length(circle, theta, radians=True):
-    if radians:
-        return theta * circle.radius
-    else:
-        theta_radians = math.radians(theta)
-        return theta_radians * circle.radius
-
-
-def bounding_box(circle):
-    xmin = circle.position[0] - circle.radius
-    xmax = circle.position[0] + circle.radius
-    ymin = circle.position[1] - circle.radius
-    ymax = circle.position[1] + circle.radius
-    return xmin, ymin, xmax, ymax
+    def bounding_box(self):
+        xmin = self.position[0] - self.radius
+        xmax = self.position[0] + self.radius
+        ymin = self.position[1] - self.radius
+        ymax = self.position[1] + self.radius
+        return xmin, ymin, xmax, ymax
 
 
 def main():
     # instantiate
     circle = Circle(radius=5)
-    print(f"The area is {area(circle)}")
+    # print(f"The area is {area(self)}")
     print(circle)
     circle.position = (30, -50)
     print(circle)
@@ -53,15 +49,15 @@ def main():
     print(f"My area is {circle.area}{circle.units}^2.")
 
     # function that calculates area
-    print(f"The area is {area(circle)}")
+    print(f"The area is {circle.area()}")
 
     # perimeter
-    print(f"{perimeter(circle) = }")
+    print(f"{circle.perimeter() = }")
     # arc length
-    print(f"{arc_length(circle, 0.8) = }")
+    print(f"{circle.arc_length(0.8) = }")
 
     # bounding box
-    print(f"{bounding_box(circle) = }")
+    print(f"{circle.bounding_box() = }")
     return 0
 
 
