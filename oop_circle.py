369c74221dd5d1dd545c2405d2c83eeba73e522a
paulkorir <paul@sci2pro.com>
2025-09-10 18:08:53 +0300

    added a rectangle class as well as attributes for fill and stroke for both circle and rectangle

--- oop_circle.py
+++ oop_circle.py
@@ -5,9 +5,11 @@ import sys
 class Circle:
     units = 'cm'
 
-    def __init__(self, radius, position=(0, 0)):
+    def __init__(self, radius, fill="white", stroke="black", position=(0, 0)):
         self.radius = radius
         self.position = position
+        self.fill = fill
+        self.stroke = stroke
         # create two attributes 'diameter' and 'area'
         self.diameter = 2 * radius
         # self.area = 3.142 * self.radius * self.radius
@@ -29,11 +31,35 @@ class Circle:
             return theta_radians * self.radius
 
     def bounding_box(self):
-        xmin = self.position[0] - self.radius
-        xmax = self.position[0] + self.radius
-        ymin = self.position[1] - self.radius
-        ymax = self.position[1] + self.radius
-        return xmin, ymin, xmax, ymax
+        x_min = self.position[0] - self.radius
+        x_max = self.position[0] + self.radius
+        y_min = self.position[1] - self.radius
+        y_max = self.position[1] + self.radius
+        return x_min, y_min, x_max, y_max
+
+class Rectangle:
+    def __init__(self, width, height, fill="white", stroke="black", position=(0, 0)):
+        self.width = width
+        self.height = height
+        self.fill = fill
+        self.stroke = stroke
+        self.position = position
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return self.width * self.height * 2
+
+    def bounding_box(self):
+        x_min = self.position[0] - self.width / 2
+        x_max = self.position[0] + self.width / 2
+        y_min = self.position[1] - self.height / 2
+        y_max = self.position[1] + self.height / 2
+        return x_min, y_min, x_max, y_max
+
+    def diagonal(self):
+        return math.sqrt(self.width ** 2 + self.height ** 2)
 
 
 def main():
