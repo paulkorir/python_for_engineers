b95f886bdeff33e9912fd33833fc5fb9768c223b
paulkorir <paul@sci2pro.com>
2025-09-10 18:13:02 +0300

    added square which inherits from rectangle

--- oop_circle.py
+++ oop_circle.py
@@ -75,6 +75,11 @@ class Text:
         self.position = position
 
 
+class Square(Rectangle):
+    def __init__(self, width, fill="white", position=(0, 0)):
+        super().__init__(width, width, fill=fill, position=position)
+
+
 def main():
     # instantiate
     circle = Circle(radius=5)
